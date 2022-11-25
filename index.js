f6286c46bfe185f63c51a2d9443e963a14a92c20
BURNI80 <alvarogutierrezc6@gmail.com>
2022-11-25 12:37:51 +0100

    Comm

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT;
 
 //New imports
 const http = require('http').Server(app);
@@ -12,7 +12,7 @@ const socketIO = require('socket.io')(http, {
     }
 });
 
-app.use(cors());
+app.use(cors())
 
 
 var num = 100
