058921d81ef932b12eb1d057c18045bc938dac39
BURNI80 <alvarogutierrezc6@gmail.com>
2023-01-09 16:52:19 +0100

    Mod

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
-// const PORT = process.env.PORT || 3000;
-const PORT = 3002;
+const PORT = process.env.PORT || 3000;
+// const PORT = 3002;
 const urlApi = "https://apitimersagc.azurewebsites.net/";
 
 //New imports
